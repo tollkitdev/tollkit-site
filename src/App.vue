5b7b58b7be779ec6e4225bc49f3b25aa23056327
Ian Davis <iandavis6481@gmail.com>
2025-04-03 22:29:56 -0500

    nav position fix

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import TopNavSection from '@/components/layout/TopNavSection.vue'
 </script>
 
 <template>
+  <TopNavSection class="sticky top-0" />
   <div class="w-full min-h-screen overflow-x-hidden">
     <div class="max-w-screen w-full overflow-x-hidden">
-      <TopNavSection class="sticky top-0" />
       <router-view></router-view>
       <FooterSection />
     </div>
